51ea7ca41f9efc58cf36cd28e2030876c0b4d252
Felipe Sabogal <david.sabogal1@gmail.com>
2016-02-28 16:43:00 -0500

    Inicialización buffer

--- Caso1InfraComp/src/Buffer.java
+++ Caso1InfraComp/src/Buffer.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 
 public class Buffer extends Thread
 {
 	//Atributos
+	private ArrayList buff;
+	private int capacidad;
+	Object lleno,vacio;
 	
 	//Constructor
 	public Buffer(int Capacidad)
 	{
-		
+		this.capacidad=Capacidad;
+		buff= new ArrayList();
+		lleno= new Object();
+		vacio= new Object();
 	}
 
 }
